ecdbfb70d421333891d4992e004fa46248575c6d
Ridercogswell <ridercogswell@gmail.com>
2022-07-18 20:07:13 -0700

    first draft manUnited

--- build/index.js
+++ build/index.js
@@ -15,3 +15,13 @@ const matches = fs_1.default.readFileSync('football.csv', {
     return row.split(',');
 });
 console.log(matches);
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games!`);
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,16 @@ const matches = fs.readFileSync('football.csv', {
 })
 
 console.log(matches);
+
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games!`);
