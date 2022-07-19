9521be98fe57762e396911a5c86b443f507e9231
Ridercogswell <ridercogswell@gmail.com>
2022-07-18 19:53:44 -0700

    generate and parse data

--- build/index.js
+++ build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('hi');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    // telling readFileSync what type of content to expect in football.csv
+    // adding this will make it return a string
+    encoding: 'utf-8'
+    // split on each new line
+}).split('\n')
+    // map over and on each row, 
+    .map((row) => {
+    return row.split(',');
+});
+console.log(matches);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-console.log('hi');
+import fs from 'fs';
 
+const matches = fs.readFileSync('football.csv', {
+  // telling readFileSync what type of content to expect in football.csv
+  // adding this will make it return a string
+  encoding: 'utf-8'
+  // split on each new line
+}).split('\n')
+// map over and on each row, 
+.map((row: string): string[] => {
+  return row.split(',');
+})
+
+console.log(matches);
