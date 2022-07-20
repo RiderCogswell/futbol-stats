3b70bce1d4d500fb4d8379a57b93bd4e9712b6a5
Ridercogswell <ridercogswell@gmail.com>
2022-07-19 21:04:39 -0700

    make abastract

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
-const utils_1 = require("./utils");
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
@@ -24,18 +23,6 @@ class CsvFileReader {
         })
             // better model
             .map(this.mapRow);
-    }
-    // isolate custom logic to mapRow func
-    mapRow(row) {
-        return [
-            (0, utils_1.dateStringToDate)(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5],
-            row[6]
-        ];
     }
 }
 exports.CsvFileReader = CsvFileReader;
