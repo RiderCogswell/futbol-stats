92885c9102becd2eaa553412e12257cedaeffe4f
Ridercogswell <ridercogswell@gmail.com>
2022-07-20 17:51:45 -0700

    refactor to inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// create object that satisfies datareader interface
+const csvFileReader = new CsvFileReader('football.csv')
+// create an instance of matchreader and pass in something that saatisfies the data reader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
-console.log(reader.data[0][0]); // this is very incredible, study it later
+console.log(matchReader.matches[0][0]); // this is very incredible, study it later
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
